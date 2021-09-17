b2725c93db1ef519dcdc517973b60cecb89a05b9
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-09-17 22:08:00 +0300

    pycodestyle fix

--- game.py
+++ game.py
@@ -14,28 +14,28 @@ def intro():
     print_pause('You find yourself standing in an open field,'
                 ' filled with grass and yellow wildflowers.')
     print_pause(f'Rumor has it that a {creature} is somewhere around here,'
-                'and has been terrifying the nearby village.')
+                ' and has been terrifying the nearby village.')
 
 
 def field(creature, tool, weapon):
     print_pause('In front of you is a house. ')
     print_pause('To your right is a dark cave. ')
     print_pause('In your hand you hold your trusty'
-                f'(but not very effective) {tool}. ')
+                f' (but not very effective) {tool}. ')
 
 
 def cave(creature, tool, weapon):
-    print_pause('You peer cautiously into the cave.  ')
+    print_pause('You peer cautiously into the cave.')
 
     if 'sword' in weapon:
         print_pause('''You've been here before, and gotten all'''
-                    '''the good stuff. It's just an empty cave now. ''')
+                    ''' the good stuff. It's just an empty cave now. ''')
     else:
         print_pause('It turns out to be only a very small cave. ')
         print_pause('Your eye catches a glint of metal behind a rock. ')
         print_pause('You have found the big sword! ')
-        print_pause(f'You discard your silly old'
-                    '{tool} and take the sword with you. ')
+        print_pause(f'You discard your silly old '
+                    f'{tool} and take the sword with you. ')
         print_pause('You walk back out to the field. ')
         weapon.append('sword')
     move(creature, tool, weapon)
@@ -43,19 +43,19 @@ def cave(creature, tool, weapon):
 
 def house(creature, tool, weapon):
     print_pause('You approach the door of the house.')
-    print_pause(f'You are about to knock when the'
-                'door opens and out steps a {creature}. ')
+    print_pause('You are about to knock, when the '
+                f'door opens and out steps a {creature}. ')
     print_pause(f'''Eep! This is the {creature}'s house! ''')
     print_pause(f'The {creature} attacks you! ')
     if 'sword' in weapon:
         print_pause(f'As the {creature} moves to attack,'
-                    'you unsheath your new sword.  ')
+                    ' you unsheath your new sword.  ')
         print_pause(f'The big sword shines brightly in your'
-                    'hand as you brace yourself for the attack.  ')
+                    ' hand as you brace yourself for the attack.  ')
         print_pause(f'But the {creature} takes one look at'
-                    'your shiny new toy and runs away! ')
+                    ' your shiny new toy and runs away! ')
         print_pause(f'You have rid the town of the {creature}.'
-                    'You are victorious!  ')
+                    ' You are victorious!  ')
         # play again?
         print_pause('Would you like to play again? (y/n)')
         new_game = input('Type    y    for a new game.\n'
@@ -67,7 +67,7 @@ def house(creature, tool, weapon):
             print_pause('Ok! See you later! Bye for now!')
     else:
         print_pause(f'You feel a bit under-prepared for this,'
-                    'what with only having a tiny {tool}. ')
+                    ' what with only having a tiny {tool}. ')
         attack(creature, tool, weapon)
 
 
@@ -87,13 +87,13 @@ def attack(creature, tool, weapon):
                          'Enter 2 to fight. \n')
     if run_or_fight == '1':
         print_pause('''You run back into the field. Luckily,'''
-                    '''you don't seem to have been followed. ''')
+                    ''' you don't seem to have been followed. ''')
         field(creature, tool, weapon)
         move(creature, tool, weapon)
 
     elif run_or_fight == '2':
         print_pause('You do your best... ')
-        print_pause(f'but your dagger is no match for the {creature}. ')
+        print_pause(f'But your dagger is no match for the {creature}. ')
         print_pause('You have been defeated! ')
         # play again?
         print_pause('Would you like to play again? (y/n)')
